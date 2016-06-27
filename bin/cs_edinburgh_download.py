24e1302e7f8f6eaf8881761fd62aea992df2defb
Margus Lukk <margus.lukk@cruk.cam.ac.uk>
2016-06-27 17:33:50 +0100

    Some more bug fixes and small improvements.

--- bin/cs_edinburgh_download.py
+++ bin/cs_edinburgh_download.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python
 #
 # $Id$
 
@@ -13,9 +13,12 @@ import re
 from subprocess import Popen, PIPE
 
 # set up logger
-from osqpipe.pipeline.setup_logs import configure_logging
+from osqutil.setup_logs import configure_logging
 from logging import INFO
 LOGGER = configure_logging(level=INFO)
+
+# import config
+from osqutil.config import Config
 
 # set up cluster job submitter
 from osqutil.cluster import ClusterJobSubmitter
@@ -36,22 +39,17 @@ DBCONF = Config()
 ## There are multiple entry points to this code
 
 ## Note that the code depends on following variables defined in config:
-# auser - aspera username (to be determined.
+# acredfile - containing credentials for connecting to Edinburgh
 # ahost - host of the aspera server in Ediburng (edgen-dt.rdf.ac.uk)
 # apport - aspera port for TCP communication
 # aoport - aspera port for UDP communication
 # arate - aspera download rate. E.g. 500M = 500Mbit/s
 # athreads - number of parallel downloads to execute
 
-# CHECKLIST BEFORE THIS CODE CAN BE TAKEN TO PRODUCTION
-# 0. Check that userid will be the same as donumber
-# 1. Has aspera username (auser) in config file been set correctly?
-# 2. Are aspera ports correct
-# 3. Is aspera project alone enough to build correct links to the files?
-# 4. Download files for one [userid]
-# 5. If previous was successful, execute download for 2 [userids] in a file configuring number of athreads to be 1.
-# 6. Test further downloads with different number of athreads
-# 7. Take code to production and download the rest of the data with maximum number of athreads that the bandwith can take
+## TODO:
+# 1. Test the script with -l and -i options. The script has been tested with -f and -F options.
+# 2. Test the script with -i option with different number of threads. See the athread option in the config file.
+# 3. Check that the logging of failed download commands to a file specified in config as 'faileddownloads' is working fine.
 
 def read_credentials(credentials_file):
     '''Reads credentials from a file to memory.'''
@@ -60,10 +58,11 @@ def read_credentials(credentials_file):
     # password\tpassword str
     # 
     credentials = {'username':None, 'password':None}
-    fh = open(credentials_file,'r')
+    fh = open(os.path.join(os.environ['HOME'], credentials_file),'rb')
     for line in fh:
+        line = line.rstrip('\n')
         cols = line.split('\t')
-        if cols[0] in self.credentials:
+        if cols[0] in credentials:
             credentials[cols[0]] = cols[1]
     for key in credentials:
         if credentials is None:
@@ -77,8 +76,27 @@ def compute_md5(fn):
         md5 = _checksum_fileobj(fileobj)
     return md5
 
-import re
-from subprocess import Popen, PIPE
+def run_command(cmd, shell=False, path=None):
+    '''A simple wrapper for running a command cmd for cases where using utilities.call_subprocess is not appropriate.
+    Agnostic whether running command was successful. Returns list of length 3 containing stdout, stderr and retcode'''
+    
+    # Set our PATH environmental var to point to the desired location.                                                                                          
+    oldpath = os.environ['PATH']
+    if path is not None:
+        if type(path) is list:
+            path = ":".join(path)
+        os.environ['PATH'] = path
+    else:
+        LOGGER.warn("Subprocess calling external executable using undefined $PATH.")
+
+    subproc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=shell)
+    (stdout, stderr) = subproc.communicate()
+    retcode = subproc.wait()
+
+    # switch environment back to what it was before.
+    os.environ['PATH'] = oldpath
+
+    return (stdout, stderr, retcode)
 
 def parse_read_header(fn):
     '''Extracts flowcell from the name of the first read in the file. Assumes either uncompressed, gzipped or bzipped fastq input file'''
@@ -92,9 +110,7 @@ def parse_read_header(fn):
     cmd = cmd + ' | head -n 1'
     
     # Run command
-    subproc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
-    (stdout, stderr) = subproc.communicate()
-    retcode = subproc.wait()
+    (stdout, stderr, retcode) = run_command(cmd, shell=True)
     
     # stderr is likely to contain error trown by zcat or bzcat due to head existing after reading only one line.
     # e.g. 'gzip: stdout: Broken pipe' or 'bzcat: I/O or other error, bailing out. Possible reason follows.'
@@ -108,9 +124,6 @@ def parse_read_header(fn):
             return (machine,flowcell,flowlane)
     sys.exit("Failed extracting flowcell information from file '%s'\n" % fn)
 
-
-
-
 # 1. Given [USER ID], construct links to all files
 
 class ed_downloader(object):
@@ -119,15 +132,15 @@ class ed_downloader(object):
         # Set maximum number of attempts to download any single file
         self.maxattempts = maxattempts
         
-        # Get following values from conf file        
-        self.auser = DBCONF.auser
+        # Get following values from conf file
         self.ahost = DBCONF.ahost
         self.aPport = DBCONF.apport
-        self.aoport = DBCONF.aoport
+        self.aOport = DBCONF.aoport
         self.arate = DBCONF.arate
         self.athreads = DBCONF.athreads
         self.destination = DBCONF.incoming
-        self.failedcommands = DBCONF.faileddownloads
+        self.failedcommands = os.path.join(DBCONF.incoming, DBCONF.faileddownloads)
+        self.path = DBCONF.clusterpath
         
         # Load aspera credentials
         self.credentials = read_credentials(DBCONF.acredfile)
@@ -188,13 +201,15 @@ class ed_downloader(object):
             fname = "%s_%s" % (userid, fprefix)
             
             attempts = 0
-            while ed_get_file(fname, project) is None:
+            while ed_get_file_with_md5(fname, project) != 0:
                 attempts += 1
                 if attempts == self.maxattempts:
                     # Record failed command
                     cmd  = 'echo cs_edinburgh_download.py -l %s -p %s >> %s' % (userid, project, self.failedcommands)
-                    call_subprocess(cmd, shell=True, path=self.destination)
+                    run_command(cmd, shell=True)
                     failed = True
+                else:
+                    LOGGER.error("Trying to download %s again." % fname)
         if failed == False:
             # Get flowcell and machine info from read header of fastq file
             (machine, flowcell, flowlane) = parse_read_header(fqfile1)
@@ -202,41 +217,66 @@ class ed_downloader(object):
             cmd = 'communicateStatus.py --status complete --flowcell %s --flowlane 0 --library %s --facility EDG' % (flowcell, userid)
             call_subprocess(cmd, shell=True, path=self.destination)
                     
-    def ed_get_file(self, fname, project):
-        '''Initiates download for fname and fname.md5 for the project. Returns fname has been downloaded and matches the md5.'''
+    def ed_get_file_with_md5(self, fname, project):
+        '''Initiates download for fname and fname.md5 for the project. Returns 0 in case downloaded fname matches downloaded md5.'''
 
         # NB! Note that the code below is agnostic to whether dowloading of the file or md5 file may have failed.
         
-        ret = None
+        ret = 0
         
         # download file
-        fn = ed_download_file(project + '/' + fname)
+        fn = os.path.join(self.destination, fname)
+        if not os.path.exists(fn + '.done'):
+            ret = self.ed_download_file(fname, project, self.destination)
+            if ret > 0:
+                return ret
+        else:
+            LOGGER.info("Skipping download for %s. File already exists." % fname)
         # download md5 for the file
-        fnmd5 = ed_download_file(project + '/' + fname + '.md5')
+        ret = self.ed_download_file(fname + '.md5', project, self.destination)
+        if ret > 0:
+            return ret
+        fnmd5 = os.path.join(self.destination, fname + '.md5')
+
         # compute md5 for the downloaded file
         md5 = compute_md5(fn)
         with open(fnmd5, 'rb') as fh:
             for line in fh:
                 line = line.rstrip('\n')
                 if line != md5:
-                    ret = None
+                    ret = 1
                 else:
-                    ret = fn
+                    ret = 0
                     break
-        # Mark file on disk as downloaded successfully
-        if ret is not None:
-            cmd = 'touch %s.done' % fname
-            call_subprocess(cmd, shell=True, path=self.destination)            
         return ret
 
-    def ed_download_file(self, fname):
-        '''Downloads file with name fname'''
+    def ed_get_file(self, fname, project):
+        '''Initiates download for fname in project.'''
 
-        for f in data_files:
-            acmd = "ascp -q -T -p -P %s -O %s -l %s %s@%s:%s %s" % (self.aPport, self.aOport, self.arate, self.credentials['username'], self.ahost, fname, self.destination)        
-            call_subprocess(acmd, shell=True, path=self.destination) # Make sure ASPERA_SCP_PASS is available in shell. 
-            fpath = os.path.join(self.destination, fname)
-        return fpath
+        # download file
+        return self.ed_download_file(fname, project, self.destination)
+
+    def ed_download_file(self, fname, project, destination):
+        '''Downloads file with name fname'''
+        
+        LOGGER.info("Downloading file %s" % fname)
+        rfpath = fname
+        if project is not None and len(project):
+            rfpath = project + '/' + fname
+        acmd = "ascp -q -T -p -P %s -O %s -l %s %s@%s:%s %s" % (self.aPport, self.aOport, self.arate, self.credentials['username'], self.ahost, rfpath, destination)
+        (stdout, stderr, retcode) = run_command(acmd, shell=True, path=self.path)
+        # call_subprocess(acmd, shell=True, path=self.path) # Make sure ASPERA_SCP_PASS is available in shell.     
+        if retcode == 0:
+            lfpath = os.path.join(destination, fname)
+            LOGGER.info("Download complete. File location: %s" % lfpath )
+            # Mark file on disk as downloaded successfully
+            cmd = 'touch %s.done' % lfpath
+            LOGGER.info("Touching file \'%s\'" % cmd )
+            (stdout, stderr, retcode) = run_command(cmd, shell=True, path=self.path)
+        else:
+            LOGGER.error("Download failed! Could it be that file %s does not exist in the server?" % rfpath)
+
+        return retcode
 
 
 ##################  M A I N   P R O G R A M  ######################
@@ -246,11 +286,12 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     
     PARSER = ArgumentParser(description='Submission of project related files to ENA')
-    PARSER.add_argument('-p', '--project', dest='project', type=str, help='Project name in Edinburgh.', required=True)
-    GROUP = parser.add_mutually_exclusive_group()
-    GROUP.add_argument('-i', '--input', dest='fin', type=str, help='Input file containing library identifiers (donumbers) one per line. \'latest_plate_donumbers.txt\'')
-    GROUP.add_argument('-l', '--library', dest='library', type=str, help='Library identifier (donumber, from Edinburgh systems point of view userid). E.g. \'do9555\'')
-    GROUP.add_argument('-f', '--file', dest='fname', type=str, help='File name in Edinburgh system. E.g. \'do9555_R1.fastq.gz\'')
+    PARSER.add_argument('-p', '--project', dest='project', type=str, help='Name of the project (subdirectory) in Edinburgh aspera server where the library files are located.', required=False, default='')
+    GROUP = PARSER.add_mutually_exclusive_group()
+    GROUP.add_argument('-i', '--input', dest='fin', type=str, help='Input file containing library identifiers (donumbers) one per line. This option is used for batch download.')
+    GROUP.add_argument('-l', '--library', dest='library', type=str, help='Library identifier (donumber, from Edinburgh systems point of view userid). E.g. \'do9555\'. This option is used for downloading data only for a specific library.')
+    GROUP.add_argument('-f', '--file', dest='fname', type=str, help='Download file and file.md5 in the project subdirectory. A file.done is created in file system on success.')
+    GROUP.add_argument('-F', '--single_file', dest='single_file', type=str, help='Download file in the project subdirectory.')
     
     # PARSER.add_argument('-s', '--split', dest='split', help='Files in the input have been demultiplexed per lane.', action='store_true')
 
@@ -264,5 +305,8 @@ if __name__ == '__main__':
     elif ARGS.library:        
         edd.ed_get_files_by_userid(userid=ARGS.library, project=ARGS.project)
     # Process by file
+    elif ARGS.fname:
+        edd.ed_get_file_with_md5(fname=ARGS.fname, project=ARGS.project)
     else:        
-        edd.ed_get_file(fname=ARGS.fname, project=ARGS.project)
+        edd.ed_get_file(fname=ARGS.single_file, project=ARGS.project)
+            
