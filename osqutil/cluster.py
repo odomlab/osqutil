ec7e469b9bb92859645ae89e28485719e62f2c90
Margus Lukk <margus.lukk@cruk.cam.ac.uk>
2017-07-25 13:46:52 +0100

    Single end alignment pipe fixes

--- osqutil/cluster.py
+++ osqutil/cluster.py
@@ -546,7 +546,7 @@ class RemoteJobRunner(JobRunner):
                                 quote(destfile))]
       cmd = " ".join(cmdbits)
 
-      LOGGER.debug(cmd)
+      LOGGER.info(cmd)
       if not self.test_mode:
         call_subprocess(cmd, shell=True, path=self.conf.hostpath)
 
@@ -1277,9 +1277,9 @@ class BwaAlignmentManager(AlignmentManager):
     picard_common_args = ('VALIDATION_STRINGENCY=SILENT', 'COMPRESSION_LEVEL=0')
     
     # Create named pipes for running commands in npiper
-    p1 = "%s_p1" % fqname
-    p2 = "%s_p2" % fqname
-    p3 = "%s_p3" % fqname
+    p1 = "%s_p01" % fqname
+    p2 = "%s_p02" % fqname
+    p3 = "%s_p03" % fqname
 
     cmd = "mknod %s p && mknod %s p && mknod %s p && sleep 1" % (p1, p2, p3)
     
@@ -1387,9 +1387,9 @@ class BwaAlignmentManager(AlignmentManager):
     picard_common_args = ('VALIDATION_STRINGENCY=SILENT', 'COMPRESSION_LEVEL=0')
           
     # Create named pipes for running commands in npiper
-    p1 = "%s_p1" % fqnames[0]
-    p2 = "%s_p2" % fqnames[0]
-    p3 = "%s_p3" % fqnames[0]
+    p1 = "%s_p01" % fqnames[0]
+    p2 = "%s_p02" % fqnames[0]
+    p3 = "%s_p03" % fqnames[0]
 
     cmd += "mknod %s p && mknod %s p && mknod %s p && sleep 1" % (p1, p2, p3)
 
