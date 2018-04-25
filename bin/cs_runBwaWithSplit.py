759385e2e8df82d0a4edd9eeec04d61f0d5ba44c
Tim Rayner <tim.rayner@cruk.cam.ac.uk>
2018-04-25 12:52:28 +0100

    Adding license notices to all code files, as recommended by the GNU GPL howto: https://www.gnu.org/licenses/gpl-howto.html

--- bin/cs_runBwaWithSplit.py
+++ bin/cs_runBwaWithSplit.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 #
-# $Id$
+# Copyright 2018 Odom Lab, CRUK-CI, University of Cambridge
+#
+# This file is part of the osqutil python package.
+#
+# The osqutil python package is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The osqutil python package is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the osqutil python package.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 """ Run BWA using Split to improve speed """
 __author__ = "Margus Lukk"
